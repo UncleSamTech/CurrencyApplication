17635c84913d3450306ffe3f9b26ea12597b923b
Samuel Joel <31622538+UncleSamTech@users.noreply.github.com>
2018-08-24 23:57:28 +0100

    MainActivity.java
    Updated my java class to implement swipe to delete

--- MainActivity.java
+++ MainActivity.java
@@ -7,6 +7,7 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -29,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<CountryDetailsObjectClass> countryDetailsObjectClasses = new ArrayList<>();
     private ConnectivityManager connMgr;
     private CountrySwipeToDeleteClass swipeToDeleteClass;
-
+    private FrameLayout frame;
+int frame_id = R.id.frame_lay;
 
 
     @Override
@@ -42,50 +44,42 @@ public class MainActivity extends AppCompatActivity {
          */
         listView = findViewById(R.id.lv_country);
         textViewErrorView = findViewById(R.id.text_view_error_display);
+        frame = findViewById(R.id.frame_lay);
+
         progressBar = findViewById(R.id.progress_bar_country);
+        progressBar = new ProgressBar(this);
         //the progress bar is set to show a display to the user while the loading is going on
-        progressBar.setVisibility(View.VISIBLE);
+
+
 //the ConnectivityManager object is referenced and called on the getSystemService method
-        connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        //the NetworkInfo class gets the current state of the device network connection
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
-        /**
-         * a check is carried out to see if there is internet or slight connection
-         * when the check is true a success message is rendered and the
-         * execute method is called on CountryDetailsAsyncTask class
-         */
-        if (networkInfo != null && networkInfo.isConnected() && networkInfo.isConnectedOrConnecting()) {
-
-            Toast.makeText(MainActivity.this, getString(R.string.msg_internet_connected), Toast.LENGTH_SHORT).show();
-            new CountryDetailsAsyncTask().execute(QueryUtilClass.JSON_RESPONSE);
-            progressBar.setVisibility(View.GONE);
-
-        }
-
-        else{
-            progressBar.setVisibility(View.GONE);
-            textViewErrorView.setText(R.string.error_msg_connected);
-        }
+            connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            //the NetworkInfo class gets the current state of the device network connection
+            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
 
-        swipeToDeleteClass = new CountrySwipeToDeleteClass(listView, new CountrySwipeToDeleteClass.DismissCallbacks() {
-            @Override
-            public boolean canDismiss(int position) {
-                return true;
+
+            /**
+             * a check is carried out to see if there is internet or slight connection
+             * when the check is true a success message is rendered and the
+             * execute method is called on CountryDetailsAsyncTask class
+             */
+            if (networkInfo != null && networkInfo.isConnected()) {
+                progressBar.setVisibility(View.VISIBLE);
+
+                Toast.makeText(MainActivity.this, getString(R.string.msg_internet_connected), Toast.LENGTH_SHORT).show();
+                new CountryDetailsAsyncTask().execute(QueryUtilClass.JSON_RESPONSE);
+                progressBar.setVisibility(View.GONE);
+
+
+            } else {
+                progressBar.setVisibility(View.GONE);
+                textViewErrorView.setText(R.string.error_msg_connected);
             }
 
-            @Override
-            public void onDismiss(ListView listView, int[] reverseSortedPositions) {
-for(int positions : reverseSortedPositions){
-    countryDetailsObjectClasses.remove(positions);
-    countryDetailsAdapterClass.notifyDataSetChanged();
 
-}
-            }
-        });
 
-        listView.setOnTouchListener(swipeToDeleteClass);
+
 /**
  * if check is false(i.e no internet connection or phone wifi off or phone in airplane mode)
  * the progress bar disappears and the textview shows the error message
@@ -118,19 +112,56 @@ for(int positions : reverseSortedPositions){
 
         @Override
         protected ArrayList<CountryDetailsObjectClass> doInBackground(String... strings) {
-            ArrayList<CountryDetailsObjectClass> countryDetailsObjectClasses =  null ;
+            ArrayList<CountryDetailsObjectClass> countryDetailsObjectClasses;
             if(QueryUtilClass.JSON_RESPONSE==null){
                 return null;
             }
+
             countryDetailsObjectClasses = QueryUtilClass.fetchCountryData(QueryUtilClass.JSON_RESPONSE);
             return countryDetailsObjectClasses;
         }
 
         @Override
-        protected void onPostExecute(ArrayList<CountryDetailsObjectClass> countryDetailsObjectClasses) {
+        protected void onPostExecute(final ArrayList<CountryDetailsObjectClass> countryDetailsObjectClasses) {
             super.onPostExecute(countryDetailsObjectClasses);
             countryDetailsAdapterClass = new CountryDetailsAdapterClass(context,countryDetailsObjectClasses);
             listView.setAdapter(countryDetailsAdapterClass);
+            /**
+             *  the constructor of our CountrySwipeToDeleteClass is called and all the
+             *  necessary interfaces are implemented to implement our swipe to delete functionality
+             */
+
+
+
+            swipeToDeleteClass = new CountrySwipeToDeleteClass(listView,  new CountrySwipeToDeleteClass.DismissCallbacks() {
+                @Override
+                public boolean canDismiss(int position) {
+                    return true;
+                }
+
+                @Override
+                public void onDismiss(ListView listView, int[] reverseSortedPositions) {
+                    for(int positions : reverseSortedPositions){
+
+                        countryDetailsObjectClasses.remove(positions);
+                        countryDetailsAdapterClass.notifyDataSetChanged();
+
+                    }
+                }
+
+
+
+
+            });
+
+            listView.setOnTouchListener(swipeToDeleteClass);
         }
     }
+
+
+
+
+
+
+
 }
